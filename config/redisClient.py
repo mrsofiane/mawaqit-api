cf28bbc3531cc99e516561ea37e66692f0fb25d0
Sofiane Louchene <sofianelilou@gmail.com>
2024-02-23 20:33:41 +0100

    Fix: Enable redis via env variable
    Enable and disable redis via an env var, and add dockeer compose to run both api and redis.

--- config/redisClient.py
+++ config/redisClient.py
@@ -1,2 +1,19 @@
+import os
 from redis import Redis
-redisClient = Redis(host='localhost', port=6379, decode_responses=True)
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read the environment variable USE_REDIS
+USE_REDIS = os.getenv('USE_REDIS', 'False').lower() == 'true'
+
+# Get Redis configuration from environment variables
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
+
+# Conditionally initialize Redis client
+if USE_REDIS:
+    redisClient = Redis(host=redis_host, port=redis_port, decode_responses=True)
+else:
+    redisClient = None
