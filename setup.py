1b2efadf65b9d666cf366b1de7c01c1f79dd3c00
Ahmad Said <47715383+Ahmad-Said@users.noreply.github.com>
2024-07-14 21:23:54 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,14 @@ setup(
     version='0.1',
     packages=find_packages(),
     url='https://github.com/Ahmad-Said/mawaqit-api',
-    install_requires=[],
+    install_requires=[
+        'uvicorn==0.19.0',
+        'requests==2.28.1',
+        'fastapi==0.85.1',
+        'httpx==0.23.0',
+        'pytest==7.2.0',
+        'bs4==0.0.1',
+        'redis==5.0.1',
+        'python-dotenv==1.0.1',
+    ],
 )
