09129368cc216c2b8a1358d61b179d613d415ecf
Ahmad Said <47715383+Ahmad-Said@users.noreply.github.com>
2024-07-14 21:08:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mawaqit-api',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/Ahmad-Said/mawaqit-api',
+    install_requires=[],
+)
