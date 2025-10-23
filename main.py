7d5440c7a6952e5adb11eccb54b1f5d657d183e3
YaRissi <53705290+YaRissi@users.noreply.github.com>
2025-10-23 03:26:27 +0200

    feat: add Redis support for rate limiting

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ load_dotenv()
 def create_app() -> FastAPI:
     app = FastAPI(title='Mawaqit Api', debug=False, read_root="/")
 
-    limiter = Limiter(key_func=get_remote_address, default_limits=[os.getenv("RATE_LIMIT", "60/minute")])
+    if os.getenv('USE_REDIS', 'False').lower() == 'true':
+        storage_uri = os.getenv('REDIS_URI', 'redis://localhost:6379')
+        limiter = Limiter(key_func=get_remote_address, default_limits=[os.getenv("RATE_LIMIT", "60/minute")], storage_uri=storage_uri)
+    else:
+        limiter = Limiter(key_func=get_remote_address, default_limits=[os.getenv("RATE_LIMIT", "60/minute")])
+        
     app.state.limiter = limiter
     app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
     app.add_middleware(SlowAPIMiddleware)
